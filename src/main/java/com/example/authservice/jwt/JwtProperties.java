695915f866ac53f699c262533a8baa69149305f0
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-04-04 06:26:35 +0900

    상수화 해야됨

--- src/main/java/com/example/authservice/jwt/JwtProperties.java
+++ src/main/java/com/example/authservice/jwt/JwtProperties.java
@@ -1,7 +1,7 @@
 package com.example.authservice.jwt;
 
 
-// 이걸로 반영 예정
+// 이걸로 반영 예정 !!
 public interface JwtProperties {
     String SECRET = "jwt-lol-lol";
     int EXPIRATION_TIME = 864000000; // 10일 (1/1000초)
